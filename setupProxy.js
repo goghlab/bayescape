f97f2fa72061869bb21281e60b15c782aa13b095
izzy <dom851103@gmail.com>
2023-12-10 07:07:03 -0800

    added error log in setupProxy.js

--- setupProxy.js
+++ setupProxy.js
@@ -6,9 +6,17 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: 'https://test.api.amadeus.com',
       changeOrigin: true,
-      onProxyReq: (proxyReq) => {
+      onProxyReq: (proxyReq, req, res) => {
         // Modify the User-Agent header
         proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (compatible; MyClient/1.0; +myclient@example.com)');
+      },
+      onError: (err, req, res) => {
+        // Handle errors
+        console.error('Proxy error:', err);
+        res.writeHead(500, {
+          'Content-Type': 'text/plain',
+        });
+        res.end('Something went wrong. Please try again.');
       },
     })
   );
